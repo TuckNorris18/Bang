65fd1d1a363793ccb956189d866e64ebdb975845
TuckNorris18 <63086220+TuckNorris18@users.noreply.github.com>
2020-11-04 16:23:43 -0600

    Update Bang.java
    Added randomized name, role generation

--- Bang.java
+++ Bang.java
@@ -2,25 +2,173 @@
  * This file is for the main game logic
  */
 
+import javax.swing.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Arrays;
+
 
 public class Bang {
-
-	public static void main(String[] args) {
-		Dice dice = new Dice();
-		Player player1 = new Player();
-		player1.setPlayer(2, 1);
+	static int players =0; //number of players
+	int arrows = 9; //Total arrows
+	
+	
+	 public static LinkedList<Player> makeTable(){
+         
+         Integer[] options = {4, 5, 6, 7, 8};
+         int option = JOptionPane.showOptionDialog(null, "How many players?",null, JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options,null);
+         
+         
+         option += 4;
+         //System.out.println(option);     
+         
+         LinkedList<Player> table = new LinkedList<Player>();
+         
+        
+         
+         for(int i =0; i < option; i++)
+         {
+        	 Player playerTemp = new Player();
+     		 playerTemp.setPlayer(i, 1);
+        	 table.add(playerTemp);
+         }
+         
+         return table;
+    }
+	 
+	 public static Player[] tableArray(LinkedList<Player> table)
+	 {
+		 Player[] tableArray = new Player[table.size()];
+		 int size = table.size();
+		 
+		 
+		 for(int i = 0; i < size; i++)
+		 {
+			 tableArray[i] = table.pop();
+		 }
+		 
+		 return tableArray;
+	 }
+	
+	public static void assignAffiliaton(Player[] table)
+	{
 		
-		player1.currentHand = dice.makeHand();
-		System.out.println(player1.playerName);
-		player1.dispAffiliation();
+		Integer[] positions = new Integer[table.length];
 		
-		System.out.println("Current Hand is: ");
-		for(int i =0; i < 5; i++)
+		
+		if(table.length == 4)
 		{
-			System.out.print(player1.currentHand[i] + " ");
+			positions[0] = 0;//makes sherrif
+			positions[1] = 3;//makes Renegade
+			positions[2] = 1;//makes outlaw
+			positions[3] = 1;
+		}
+		if(table.length == 5)
+		{
+			positions[0] = 0;
+			positions[1] = 3;
+			positions[2] = 1;
+			positions[3] = 1;
+			positions[4] = 2;//makes deputy
+		}
+		if(table.length == 6)
+		{
+			positions[0] = 0;
+			positions[1] = 3;
+			positions[2] = 1;
+			positions[3] = 1;
+			positions[4] = 2;
+			positions[5] = 1;
+		}
+		if(table.length == 7)
+		{
+			positions[0] = 0;
+			positions[1] = 3;
+			positions[2] = 1;
+			positions[3] = 1;
+			positions[4] = 2;
+			positions[5] = 1;
+			positions[6] = 2;
+		}
+		if(table.length == 8)
+		{
+			positions[0] = 0;
+			positions[1] = 3;
+			positions[2] = 1;
+			positions[3] = 1;
+			positions[4] = 2;
+			positions[5] = 1;
+			positions[6] = 2;
+			positions[7] = 3;
 		}
 		
-		player1.handAsText(player1.currentHand);
+		List<Integer> posList = Arrays.asList(positions);
+		Collections.shuffle(posList);
+		posList.toArray(positions);
+		//System.out.println(Arrays.deepToString(positions));	
+		
+		
+		
+		for(int i =0; i < table.length; i++)
+		{
+			table[i].setAffiliation(positions[i]);
+		}
+		
+		
+	}
+	
+	
+	public static void assignNames(Player[] table)
+	{
+		Integer[] names = new Integer[16];
+		
+		for(int i = 0; i < 16; i++)
+		{
+			names[i] = i+1;
+			//System.out.println(names[i]);
+		}
+		
+		
+		
+		List<Integer> scrambledNames = Arrays.asList(names);
+		Collections.shuffle(scrambledNames);
+		scrambledNames.toArray(names);
+		
+		for(int i =0; i < table.length; i++)
+		{
+			table[i].setName(names[i]);
+		}
+		
+	}
+	
+	public static void randomizeTable(Player[] table)
+	{
+		assignNames(table);
+		assignAffiliaton(table);
+		
+		for(int i =0; i < table.length; i++)
+		{
+			//System.out.println(table[i].playerName);
+			table[i].dispAffiliation();
+			System.out.print(" " + table[i].playerName+ "\n");
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		Dice dice = new Dice();
+		
+		
+		
+		LinkedList<Player> table = new LinkedList<Player>();
+		table = makeTable();
+		
+		Player[] tableArray = tableArray(table);
+		
+		
+		randomizeTable(tableArray);
 	}
 
 }
